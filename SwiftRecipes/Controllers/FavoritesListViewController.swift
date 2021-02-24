9d8dd1ad399a0f2186a1af048db82427d8f66e3e
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-02-24 14:39:22 +0100

    Basic tab bar and navigation controller links

---
+++ SwiftRecipes/Controllers/FavoritesListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListViewController.swift
+//  SwiftRecipes
+//
+//  Created by Tjeu Foolen on 24/02/2021.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ SwiftRecipes/Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeDetailViewController.swift
+//  SwiftRecipes
+//
+//  Created by Tjeu Foolen on 24/02/2021.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
